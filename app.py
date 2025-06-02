bf36eab6dade5a2a4b84cca22cdea3aa16ce647d
Ade Cahyaning <75601369+adecahyaning@users.noreply.github.com>
2025-06-02 10:35:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -167,4 +167,5 @@ def forminator_webhook():
 # ------------------ RUN ------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
